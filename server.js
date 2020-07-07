f6c68971e32c86016a860a11cc26f9878a9e1f66
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-07-07 21:26:59 +0530

    :hammer: fixed bug to serve app.js correctly

--- server.js
+++ server.js
@@ -8,11 +8,17 @@ const { notFound, errorHandler } = require('./middleware');
 require('dotenv').config();
 
 const app = express();
+app.enable('trust proxy');
 
 app.use(helmet());
 app.use(morgan('common'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public/')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// to serve app.js which is linked in index.html file
+app.get('/app.js', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public/app.js'));
+});
 
 app.use(notFound);
 app.use(errorHandler);

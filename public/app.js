4d7357d08d182e1fe13741d1c08c7115600a73b7
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-07-13 22:33:29 +0530

    :unicorn: added github corner + added submit method

--- public/app.js
+++ public/app.js
@@ -1,4 +1,17 @@
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    url: '',
+    code: '',
+    error: '',
+    isCreated: false,
+    formVisible: true,
+  },
+  methods: {
+    submitFN() {
+      console.log('This is some data: ', [this.url, this.code]);
+      this.formVisible = false;
+      this.isCreated = true;
+    },
+  },
 });

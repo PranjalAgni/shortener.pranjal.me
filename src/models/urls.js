a3cc761d9aa34826efe6bb560db87fa6999f0bac
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-07-30 01:13:49 +0530

    :zap: added mongodb schema

---
+++ src/models/urls.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UrlsSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    code: {
+      type: String,
+      unique: true,
+    },
+    clicks: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+const Urls = mongoose.model("Urls", UrlsSchema);
+
+module.exports = Urls;

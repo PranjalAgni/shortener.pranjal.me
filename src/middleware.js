e95a3cad317c8d46aa06bafc6f9de8fc9c15c667
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-08-02 00:01:47 +0530

    🔐 updated auth logic

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,12 @@
 const path = require("path");
 
 const authorization = (req, res, next) => {
+  const date = new Date();
+  const weekday = date.toLocaleString("default", { weekday: "long" });
+
   if (
     process.env.NODE_ENV === "development" ||
-    req.headers["x-api-key"] === "token"
+    req.headers["x-api-key"] === weekday
   ) {
     next();
   } else {

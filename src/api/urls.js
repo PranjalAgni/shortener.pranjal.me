e723f29772a7844721e28659dc3ea4585f0b6d43
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-08-02 00:00:40 +0530

    ✅ added rate limiter to the API

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
 
 const Urls = require("../models/urls");
 const { formatResponse } = require("../utils/responseFormatter");
@@ -11,48 +13,65 @@ const {
 
 const router = Router();
 
-router.post("/create", async (req, res, next) => {
-  try {
-    const { targetUrl, shortId } = req.body;
-    let shortCode = shortId;
-    let isCodeGenerated = false;
-
-    await createUrlRequestBodySchema.validate({ targetUrl, shortCode });
-
-    if (!shortCode) {
-      shortCode = generateShortCode();
-      isCodeGenerated = true;
-    }
-
-    const isCodePresent = await checkIfCodeExists(shortCode);
-
-    if (isCodePresent) {
-      let newCodeStatus = null;
-      if (isCodeGenerated) {
-        // do some retry as we generated this code
-        newCodeStatus = await retryAndGenerateUniqueCode();
-        shortCode = newCodeStatus.shortCode;
-      }
-
-      if (!isCodeGenerated || !newCodeStatus.status) {
-        throw new Error("Short code already in use. 🍔");
-      }
-    }
-
-    shortCode = shortCode.toLowerCase();
-
-    const newUrl = {
-      code: shortCode,
-      url: targetUrl,
-    };
-
-    const urlShortened = new Urls(newUrl);
-    const createdEntry = await urlShortened.save();
-    formatResponse(res, createdEntry);
-  } catch (error) {
-    if (error.name === "ValidationError") res.status(422);
-    next(error);
-  }
+const shortenUrlRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  message: "Too many URL created from this IP.",
 });
+
+const shortenUrlSpeedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 2,
+  delayMs: 500,
+});
+
+router.post(
+  "/create",
+  shortenUrlSpeedLimiter,
+  shortenUrlRateLimiter,
+  async (req, res, next) => {
+    try {
+      const { targetUrl, shortId } = req.body;
+      let shortCode = shortId;
+      let isCodeGenerated = false;
+
+      await createUrlRequestBodySchema.validate({ targetUrl, shortCode });
+
+      if (!shortCode) {
+        shortCode = generateShortCode();
+        isCodeGenerated = true;
+      }
+
+      const isCodePresent = await checkIfCodeExists(shortCode);
+
+      if (isCodePresent) {
+        let newCodeStatus = null;
+        if (isCodeGenerated) {
+          // do some retry as we generated this code
+          newCodeStatus = await retryAndGenerateUniqueCode();
+          shortCode = newCodeStatus.shortCode;
+        }
+
+        if (!isCodeGenerated || !newCodeStatus.status) {
+          throw new Error("Short code already in use. 🍔");
+        }
+      }
+
+      shortCode = shortCode.toLowerCase();
+
+      const newUrl = {
+        code: shortCode,
+        url: targetUrl,
+      };
+
+      const urlShortened = new Urls(newUrl);
+      const createdEntry = await urlShortened.save();
+      formatResponse(res, createdEntry);
+    } catch (error) {
+      if (error.name === "ValidationError") res.status(422);
+      next(error);
+    }
+  },
+);
 
 module.exports = router;

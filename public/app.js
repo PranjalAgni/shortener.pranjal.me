79afd847d34dd6a196afcaa9857fdef1909c1a86
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-08-07 00:24:33 +0530

    :green_heart: UI side changes

--- public/app.js
+++ public/app.js
@@ -1,17 +1,52 @@
+const BASE_API_URL = window.location.href;
+
 const app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    url: '',
-    code: '',
-    error: '',
+    url: "",
+    code: "",
+    error: "",
+    shortenedUrl: "",
     isCreated: false,
     formVisible: true,
   },
   methods: {
-    submitFN() {
-      console.log('This is some data: ', [this.url, this.code]);
-      this.formVisible = false;
-      this.isCreated = true;
+    async submitFN() {
+      const API_URL = `${BASE_API_URL}api/url/create`;
+      let requestPayload = {
+        targetUrl: this.url,
+        shortId: this.code || undefined,
+      };
+
+      const headers = {
+        "Content-Type": "application/json;charset=utf-8",
+        "x-api-key": this.getWeekDay(),
+      };
+
+      const response = await fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify(requestPayload),
+        headers,
+      });
+
+      const data = await response.json();
+      console.log(data);
+      this.setAPIResponse(response, data);
+    },
+    getWeekDay() {
+      const date = new Date();
+      return date.toLocaleString("default", { weekday: "long" });
+    },
+    setAPIResponse(response, data) {
+      if (response.ok) {
+        this.shortenedUrl = `${BASE_API_URL}${data.res.code}`;
+        this.isCreated = true;
+        this.formVisible = false;
+        this.error = "";
+      } else if (response.status === 422) {
+        this.error = data.message;
+        this.isCreated = false;
+      }
     },
   },
 });

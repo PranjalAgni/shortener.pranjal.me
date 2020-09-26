9734d936046f55bdb8725fbfc6d6862d04095f21
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-09-26 17:49:44 +0530

    :sparkles: feat: added sweet alerts to show the shortened url

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="main">
-    <Header />
-    <GithubCorner />
-    <div class="result-holder" v-if="status">
-      <p class="created">
-        Shortened url:
-        <a :href="shortenedUrl">{{ shortenedUrl }}</a>
-      </p>
+  <v-app class="app">
+    <div class="main">
+      <Header />
+      <GithubCorner />
+      <!-- <div class="result-holder" v-if="status">
+        <p class="created">
+          Shortened url:
+          <a :href="shortenedUrl">{{ shortenedUrl }}</a>
+        </p>
+      </div> -->
+      <Form />
     </div>
-    <Form />
-  </div>
+  </v-app>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-
 import Form from './components/Form.vue';
 import GithubCorner from './components/GithubCorner';
 import Header from './components/Header';
-
 export default {
   name: 'App',
   components: {
@@ -29,25 +29,63 @@ export default {
   computed: {
     ...mapGetters(['shortenedUrl', 'status']),
   },
+  watch: {
+    status(newStatus) {
+      if (newStatus) {
+        this.$swal({
+          titleText: `${this.shortenedUrl}`,
+          icon: 'success',
+          customClass: {
+            title: 'title-class',
+          },
+          confirmButtonText: '<v-icon>📋</v-icon>Copy',
+          confirmButtonColor: '#3A9FF3',
+          preConfirm: () => {
+            navigator.clipboard.writeText(this.shortenedUrl);
+          },
+          allowOutsideClick: () => {
+            navigator.clipboard.writeText(this.shortenedUrl);
+            return true;
+          },
+        });
+      }
+    },
+  },
 };
 </script>
 
 <style>
+@import '~@sweetalert2/theme-dark/dark.css';
+
 .result-holder {
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
+.app {
+  height: 100%;
+  width: 100%;
+}
+
 .main {
   display: block;
 }
-
 .created {
   color: #ffffff;
 }
-
 .created a {
   color: inherit;
+}
+
+.title-class {
+  align-items: center;
+  justify-content: center;
+}
+
+@media only screen and (max-width: 768px) {
+  .title-class {
+    font-size: 18px;
+  }
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
+import vuetify from './plugins/vuetify';
 import store from './store';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueSweetalert2);
 new Vue({
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');

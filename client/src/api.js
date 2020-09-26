76194c9fcd7e1fcc173011ad143d2dc357b56727
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-09-26 17:46:00 +0530

    :hammer: refactor: clean code added a generic function to do API call

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,19 @@
-import { BASE_API_URL } from './constants';
+import { BASE_API_URL } from './utils/constants';
+import { setValue } from './utils/cache';
+
+const makeAPICall = async (API_URL, payload, method = 'POST') => {
+  const headers = {
+    'Content-Type': 'application/json;charset=utf-8',
+  };
+  const response = await fetch(API_URL, {
+    method,
+    body: JSON.stringify(payload),
+    headers,
+  });
+
+  return response;
+};
+
 export const makeURLShortened = async (payload) => {
   try {
     const API_URL = `${BASE_API_URL}api/url/create`;
@@ -8,18 +23,27 @@ export const makeURLShortened = async (payload) => {
       shortId: payload.code || undefined,
     };
 
-    const headers = {
-      'Content-Type': 'application/json;charset=utf-8',
-    };
-
-    const response = await fetch(API_URL, {
-      method: 'POST',
-      body: JSON.stringify(requestPayload),
-      headers,
-    });
+    const response = await makeAPICall(API_URL, requestPayload);
 
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const checkCodeValid = async (payload) => {
+  try {
+    const API_URL = `${BASE_API_URL}api/url/search`;
+
+    const requestPayload = {
+      shortId: payload,
+    };
+    const response = await makeAPICall(API_URL, requestPayload);
+    const data = await response.json();
+    setValue(payload, true);
+    return data?.result?.present;
   } catch (error) {
     console.error(error);
     return null;
